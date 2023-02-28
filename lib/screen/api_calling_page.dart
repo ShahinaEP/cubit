2209e041c0e24e0aa0f507014e5f927c5913b786
ShahinaEP <shahinaeasminpia860703@gmail.com>
2023-02-28 13:11:45 +0600

    user cubit call api page

--- lib/screen/api_calling_page.dart
+++ lib/screen/api_calling_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit_section/user-cubit-section/user_cubit.dart';
+import '../cubit_section/user-cubit-section/user_cubit_state.dart';
 class ApiCallingPage extends StatefulWidget {
   const ApiCallingPage({Key? key}) : super(key: key);
 
@@ -15,6 +19,13 @@ class _ApiCallingPageState extends State<ApiCallingPage> {
           title:  const Text('Api Calling page'),
           centerTitle: true,
         ),
+          body: BlocBuilder<UserCubit, UserCubitState>(
+        builder: (context, state){
+          return Column(
+            children: [],
+          );
+        }
+          )
     );
   }
 }
